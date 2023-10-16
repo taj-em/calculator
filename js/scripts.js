9d18c3b917b064b7f358054a6cabb60db314287d
Your Name <tajempey@gmail.com>
2023-10-16 12:40:16 -0600

    Compile answers into one alert

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,4 @@ function divide(num1, num2) {
 const num1 = parseInt(prompt("Enter a number:"));
 const num2 = parseInt(prompt("Enter another number:"));
 
-window.alert(num1 + " added to " + num2 + " = " + add(num1, num2));
-window.alert(num1 + " subtracted by " + num2 + " = " + subtract(num1, num2));
-window.alert(num1 + " multiplied by " + num2 + " = " + multiply(num1, num2));
-window.alert(num1 + " divided by " + num2 + " = " + divide(num1, num2));
+window.alert(num1 + " added to " + num2 + " = " + add(num1, num2) + ". " + num1 + " subtracted by " + num2 + " = " + subtract(num1, num2) + ". " + num1 + " multiplied by " + num2 + " = " + multiply(num1, num2) + ". " + num1 + " divided by " + num2 + " = " + divide(num1, num2))
